3bc010d1a79a0cb8c4cb70f5fb92a50202775927
koji <kk2796@nyu.edu>
2023-06-21 01:43:03 -0400

    introduce vite plugin to reduce package size

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
+    viteCompression(),
   ],
   build: {
     lib: {
