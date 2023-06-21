6b9bd2d3cad073941c47cadea8983dedd586cc5a
koji <kk2796@nyu.edu>
2023-06-21 02:11:39 -0400

    update settings

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,15 @@
-import {} from 'styled-components/cssprop'
+import { CSSProp } from 'styled-components'
+
+declare module 'react' {
+  interface DOMAttributes<T> {
+    css?: CSSProp
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      css?: CSSProp
+    }
+  }
+}
