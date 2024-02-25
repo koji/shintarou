0c3e57e482a3ede178d01d8a4a2f74a333417644
koji <kk2796@nyu.edu>
2024-02-25 17:59:49 -0500

    fix build errors

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -2,9 +2,9 @@ module.exports = {
   framework: '@storybook/react-vite',
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  // features: {
-  //   storyStoreV7: false,
-  // },
+  features: {
+    storyStoreV7: false,
+  },
   async viteFinal(config) {
     return config
   },
